a5d2150939f4bc591e2c3cbdbb5470eb496aeb78
VladBrok <vlad.brok99@gmail.com>
2023-05-24 19:21:10 +0300

    test assert in production

--- src/app/scenes/game-scene/page.tsx
+++ src/app/scenes/game-scene/page.tsx
@@ -61,17 +61,6 @@ export default function GameScene() {
   const [isRoundLost, setIsRoundLost] = useState(false);
   const prevActivePlayerIdx = useRef<null | number>(null);
 
-  // TODO: remove
-  useEffect(() => {
-    const breakIt = () => {
-      assert(false);
-    };
-
-    document.addEventListener("click", breakIt);
-
-    return () => document.removeEventListener("click", breakIt);
-  }, []);
-
   const suitWidth = cardWidth / 2;
   const suitHeight = cardHeight / 2.5;
 
