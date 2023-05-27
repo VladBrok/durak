c706b3359f8d6e4bc4db5b0ef4e41635bf6a50fc
VladBrok <vlad.brok99@gmail.com>
2023-05-27 14:33:32 +0300

    use --vh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { RemountContextProvider } from "../context/remount-context";
+import { debounce } from "../utils/debounce";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,18 @@ export default function RootLayout({
   function remount(): void {
     setRemountKey(Math.random());
   }
+
+  useEffect(() => {
+    const setCustomVh = debounce(() => {
+      const doc = document.documentElement;
+      doc.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+    }, 100);
+
+    window.addEventListener("resize", setCustomVh);
+    setCustomVh();
+
+    return () => window.removeEventListener("resize", setCustomVh);
+  }, []);
 
   return (
     <html lang="en">
