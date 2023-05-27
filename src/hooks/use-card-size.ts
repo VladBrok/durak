ac31eca575005a71336682ae5c7f91a537ffa87b
VladBrok <vlad.brok99@gmail.com>
2023-05-27 14:47:03 +0300

    watch card size change

--- src/hooks/use-card-size.ts
+++ src/hooks/use-card-size.ts
@@ -1,14 +1,32 @@
 import { useEffect, useState } from "react";
 import { getCssVarValue } from "../utils/get-css-var-value";
+import { debounce } from "../utils/debounce";
 
 export function useCardSize(): [number, number] {
   const [cardWidth, setCardWidth] = useState(0);
   const [cardHeight, setCardHeight] = useState(0);
 
   useEffect(() => {
-    setCardWidth(getCssVarValue("--card-width"));
-    setCardHeight(getCssVarValue("--card-height"));
-  }, []);
+    const set = () => {
+      setCardWidth(getCssVarValue("--card-width"));
+      setCardHeight(getCssVarValue("--card-height"));
+    };
+
+    const setIfChanged = debounce(() => {
+      if (
+        cardWidth !== getCssVarValue("--card-width") ||
+        cardHeight !== getCssVarValue("--card-height")
+      ) {
+        set();
+      }
+    });
+
+    set();
+
+    window.addEventListener("resize", setIfChanged);
+
+    return () => window.removeEventListener("resize", setIfChanged);
+  }, [cardHeight, cardWidth]);
 
   return [cardWidth, cardHeight];
 }
